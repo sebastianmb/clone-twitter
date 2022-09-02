02392cd8b39551449f4bf719b6143837d5e283c9
sebastianmb <sebastianmb88@gmail.com>
2022-09-01 23:18:42 -0500

    Avance 90% estilos widgets

--- src/componentes/Widgets/Index.js
+++ src/componentes/Widgets/Index.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Search } from '@material-ui/icons';
+import SearchIcon from "@material-ui/icons/Search";
+import { Widget, Header, DivIcon, DivContent } from "./styles";
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
+
 
 const Widgets = () => {
   return (
-    <div>
-      
-    </div>
+    <Widget>
+      <Header>
+        <DivIcon>
+          <SearchIcon className='searchIcon' />
+          <input placeholder='Buscar en Twitter clone' />
+        </DivIcon>
+      </Header>
+
+      <DivContent>
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed
+          tweetId={'933354946111705097'}
+        />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="jehingson1"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={'https://facebook.com/saurabhnemade'}
+          options={{ text: '#reactjs is awesome', via: 'saurabhnemade' }}
+        />
+      </DivContent>
+
+    </Widget>
+
+
   )
 }
 
