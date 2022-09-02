d864f19fe8ba93e43a2feb7d93b4a2d500d2619d
sebastianmb <sebastianmb88@gmail.com>
2022-09-01 23:22:36 -0500

    Terminado componentess widgets en estilos css

---
+++ src/componentes/Widgets/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+
+export const Widget= styled.div`
+flex: 0.3;
+overflow-y: scroll;
+box.sizing: border-box;
+&::-webkit-scrollbar{
+    display: none;
+    
+}
+--ms-overflow-style:none;
+scrollbar-width:none;
+`
+
+export const Header= styled.div`
+background: white;
+heigth: 50px;
+position: sticky;
+paddin-top: 10px;
+top:o;
+`
+
+export const DivIcon= styled.div`
+display: flex;
+align-items: center;
+background-color: #eee;
+padding:10px;
+border-radius: 20px;
+>searchIcon{
+    color:#5b7083;
+
+}
+>input{
+    border:none;
+    outline:0;
+    padding-left: 10px;
+    background-color: #eee;
+    font-size:16px;
+}
+`
+export const DivContent= styled.div`
+`  
